a8bfbcc2f9ed1464019ed380dfdf4a53f0d73c85
Abstract-a <m.kamari100@gmail.com>
2024-12-21 20:17:18 +0100

    wip added tailwind classes

--- src/components/comments/AddComment.vue
+++ src/components/comments/AddComment.vue
@@ -5,20 +5,20 @@ let loading = ref(false);
 let comment = ref('');
 </script>
 <template>
-  <div className="flex w-full gap-4">
-    <form className="flex w-full">
+  <div class="flex w-full gap-4">
+    <form class="flex w-full">
       <input
         type="text"
         placeholder="Write a comment"
         id="comment"
         v-model="comment"
         required
-        className="m-3 w-[80%] rounded-md p-3"
+        class="m-3 w-[80%] rounded-md p-3"
       />
       <button
         type="submit"
         disabled="loading"
-        className="m-3 cursor-pointer rounded-lg border-none bg-green-500 px-6 text-white transition-all duration-500 ease-in-out hover:bg-green-600 hover:opacity-90"
+        class="m-3 cursor-pointer rounded-lg border-none bg-green-500 px-6 text-white transition-all duration-500 ease-in-out hover:bg-green-600 hover:opacity-90"
       >
         {{ loading ? 'Saving' : 'Save' }}
       </button>
--- src/components/comments/CommentsPage.vue
+++ src/components/comments/CommentsPage.vue
@@ -10,16 +10,13 @@ let comments = ref([]);
 
 <template>
   <div
-    className="z-[1000] h-[40%] w-screen bg-gray-200 md:h-[560px] md:min-w-[384px] md:max-w-[500px] md:rounded-md"
+    class="z-[1000] h-[40%] w-screen bg-gray-200 md:h-[560px] md:min-w-[384px] md:max-w-[500px] md:rounded-md"
   >
     <BaseSpinner v-if="loading" />
-    <div
-      v-else
-      className="flex h-full w-full flex-col items-start justify-center"
-    >
+    <div v-else class="flex h-full w-full flex-col items-start justify-center">
       <AddComment />
       <div
-        className="mt-4 flex h-full w-full flex-col items-start justify-start overflow-auto overflow-x-hidden"
+        class="mt-4 flex h-full w-full flex-col items-start justify-start overflow-auto overflow-x-hidden"
       >
         <SingleComment v-for="comment in comments" :key="comment._id" comment />
       </div>
