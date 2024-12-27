db5087a9ed5beb0d58acb65031120671d8ffb666
Abstract-a <m.kamari100@gmail.com>
2024-12-27 13:45:01 +0100

    changed some colors

--- src/components/todos/SingleTodo.vue
+++ src/components/todos/SingleTodo.vue
@@ -74,6 +74,7 @@ async function handleCompleted(e) {
           >
             >
             <path
+              class="fill-gray-700"
               d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-352a96 96 0 1 1 0 192 96 96 0 1 1 0-192z"
             />
           </svg>
@@ -85,6 +86,7 @@ async function handleCompleted(e) {
           >
             >
             <path
+              class="fill-gray-700"
               d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256-96a96 96 0 1 1 0 192 96 96 0 1 1 0-192z"
             />
           </svg>
@@ -116,7 +118,9 @@ async function handleCompleted(e) {
             >
             <path
               :class="[
-                props.todo.completed ? 'fill-gray-500 cursor-default' : '',
+                props.todo.completed
+                  ? 'fill-gray-400 cursor-default'
+                  : 'fill-gray-700',
               ]"
               d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"
             />
@@ -133,6 +137,7 @@ async function handleCompleted(e) {
           >
             >
             <path
+              class="fill-gray-700"
               d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"
             />
           </svg>
